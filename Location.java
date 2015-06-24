e25863076416335d020808373ff7aa2996b69e3c
msart <pedromarcondes94@hotmail.com>
2015-06-24 19:33:58 -0300

    Update Location.java

--- Location.java
+++ Location.java
@@ -1,19 +1,29 @@
 
 public class Location {
-    private final long latitude;
-    private final long longitude;
+    private final double latitude;
+    private final double longitude;
 
-    public Location(long latitude, long longitude) {
+    public Location(double latitude, double longitude) {
         this.latitude = latitude;
         this.longitude = longitude;
     }
 
-    public long latitude() { return latitude; }
-    public long longitude() { return longitude; }
+    public double latitude() { return latitude; }
+    public double longitude() { return longitude; }
 
     public boolean equals(Location that) {
         if      (that.latitude() == this.latitude() && that.longitude() == this.longitude()) return true;
         else return false;
+    }
+    
+    public double distance(Location that){
+        double theta = this.longitude() - that.longitude();
+        double dist = Math.sin(deg2rad(this.longitude())) * Math.sin(deg2rad(that.longitude())) + Math.cos(deg2rad(this.longitude())) * Math.cos(deg2rad(that.longitude())) * Math.cos(deg2rad(theta));
+        dist = Math.acos(dist);
+        dist = rad2deg(dist);
+        dist = dist * 60 * 1.1515;
+        dist = dist * 1.609344; // ta em kilmetros, achei na net esse metodo
+         return (dist);
     }
 
     public String toString() {
